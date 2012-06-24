3c1b5c5fd2dccb54547cd64f374f1263f123c025
triggerwoods <keithflint@inbox.ru>
2012-06-24 16:29:55 +0400

    0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qblog::Application.config.secret_token = '971784e43eb0df5a40689cffbc69c7ec35b0553e59c7bdfab95266a694e37997fd2ccfd313a80301188e1e5e9eb971381b005343001c54cd8f55073bec689ff0'
